dc0ce7dc4ee53a973931256b6d66cb877b5ac97d
egichSerg <e.sergeev@g.nsu.ru>
2024-02-22 23:58:57 +0700

    Auto commit. Files updated at Thu Feb 22 11:58:57 PM +07 2024

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ class Identity(nn.Module):
     
 encoder = torchvision.models.mobilenet_v3_small(weights=mweights.DEFAULT)
 encoder.classifier = Identity()
+encoder.avgpool = Identity()
 
 
 ### decoder definition ###
@@ -27,9 +28,12 @@ class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-
+decoder = Decoder()
 ### LSTM model definition ###
 
 class EncoderDecoderLSTM(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(EncoderDecoderLSTM, self).__init__()
+        self.rnn = nn.LSTM() #nn.LSTM(input_size, hidden_size, num_layers, dropout=p), where input_size = encoder_context_vector_shape
+        self.encoder = encoder
+        self.decoder = decoder
