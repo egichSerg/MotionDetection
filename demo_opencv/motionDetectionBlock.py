99cf0b88971d9d9dc958c634ab2e59833ab2789d
egichSerg <e.sergeev@g.nsu.ru>
2024-02-24 23:47:51 +0700

    Auto commit. Files updated at Sat Feb 24 11:47:51 PM +07 2024

--- demo_opencv/motionDetectionBlock.py
+++ demo_opencv/motionDetectionBlock.py
@@ -3,11 +3,11 @@ import cv2 as cv
 
 
 class MotionDetector:
-    def __init__(self, resolution = (640, 480), cumsum_enabled = True, delta_time = 2) -> None:
+    def __init__(self, resolution = (640, 480), forget_percentage = 50, cumsum_enabled = True, delta_time = 2) -> None:
         self.cumsum_enabled = cumsum_enabled
         self.delta_time = delta_time
         self.resolution = resolution
-        self.frames_collection = np.empty(shape=resolution)
+        self.frame_difference = np.empty(shape=[ *resolution, 3 ])
 
 
     ### set time difference in frames (for subtraction)
@@ -19,8 +19,9 @@ class MotionDetector:
 
     def set_resolution(self, resolution):
         self.resolution = resolution
-        self.frames_collection = np.empty(shape=resolution)
+        self.frame_difference = np.empty(shape=[ *resolution, 3 ])
 
     def get_difference(self, image: np.ndarray):
         if image.shape != [ 3, *self.resolution ]:
-            pass
+            raise SystemExit("Resolution of detector and image doesn't match")
+        
